13044afbc7a3b37250674531abbcd706d8ab886a
fbaiex <fabio.aiex@hotmail.com>
2023-03-19 14:53:10 -0300

    Initial Commit

---
+++ app.js
@@ -0,0 +1,12 @@
+var mostrarSite = true;
+const site = "www.stackx.com.br";
+
+console.log("Hello World!");
+console.log("Meu nome é Fabio!");
+console.log("E eu estou aprendendo Node.js com a StackX");
+
+if(mostrarSite){
+    
+    console.log(site);
+
+}
